f54963c9747593345bf0f1dfe0497ef04c95d4a4
Miracle.lin <miracle.lin@zeasn.com>
2021-05-12 21:34:13 +0800

    get()方式请求成功

--- amazing/src/main/java/com/lingong/amazinglab/ui/MainActivity.java
+++ amazing/src/main/java/com/lingong/amazinglab/ui/MainActivity.java
@@ -2,9 +2,6 @@ package com.lingong.amazinglab.ui;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,9 +11,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.lingong.amazinglab.BuildConfig;
 import com.lingong.amazinglab.R;
 import com.lingong.amazinglab.request.InternetRequest;
-import com.lingong.amazinglab.request.ResponseListener;
 import com.lingong.amazinglab.utils.AuthUtil;
 import com.lingong.amazinglab.utils.LogUtil;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class MainActivity extends AppCompatActivity {
@@ -49,106 +53,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    //新线程进行网络请求
-    Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
-//            Message msg = new Message();
-//            Bundle data = new Bundle();
-//            data.putString("value", "请求结果");
-//            msg.setData(data);
-//            handler.sendMessage(msg);
-
-            String authSign = null;
-            try {
-                authSign = AuthUtil.getAuth();
-
-                IR = new InternetRequest();
-                IR.requestGet(authSign, BuildConfig.DOWNLOAD_CERT_URL, new ResponseListener() {
-                    @Override
-                    public void onResponseResult(String result) {
-                        LogUtil.d("success onResponse = " + result);
-                    }
-
-                    @Override
-                    public void onResponseError(String result) {
-                        LogUtil.e("onFailure =" + result);
-                    }
-                });
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-        }
-    };
-    //handler 处理返回的请求结果
-    Handler handler = new Handler(Looper.myLooper()) {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            Bundle data = msg.getData();
-            String val = data.getString("value");
-            //
-            // TODO: 更新界面
-            //
-        }
-    };
-
-
 
     private void getDownloadCert() {
-        try {new Thread(runnable).start();
-//            OkHttpClient client = new OkHttpClient.Builder()
-//                    .followRedirects(false) //关闭重定向  Too many follow-up requests: 21
-//                    .followSslRedirects(false)
-//                    .authenticator((route, response) ->
-//                            response.request().newBuilder().addHeader("Authorization", authSign).build())
-//                    .build();
-//            Request request = new Request.Builder().url(BuildConfig.DOWNLOAD_CERT_URL).build();
-//            client.newCall(request).enqueue(new Callback() {
-//                @Override
-//                public void onFailure(Call call, IOException e) {
-//                    LogUtil.e("onFailure =" + e.getMessage());
-//                }
-//
-//                @Override
-//                public void onResponse(Call call, Response response) throws IOException {
-//                    LogUtil.d("success onResponse = \n" + response.body().string());
-//                }
-//            });
+        try {
+            String authSign = AuthUtil.getAuth();
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .followRedirects(false) //关闭重定向  Too many follow-up requests: 21
+                    .followSslRedirects(false)
+                    .authenticator((route, response) ->
+                            response.request().newBuilder().addHeader("Authorization", authSign).build())
+                    .build();
+            Request request = new Request.Builder().url(BuildConfig.DOWNLOAD_CERT_URL).get().build();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    LogUtil.e("onFailure =" + e.getMessage());
+                }
 
-//            client.connectTimeout(10, TimeUnit.SECONDS);
-//            Retrofit retrofit = new Retrofit.Builder()
-//                    .client(client.build())
-//                    .addConverterFactory(GsonConverterFactory.create())
-//                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-//                    .baseUrl(BASE_DOWNLOAD_URL)
-//                    .build();
-//
-//            HttpService apiService = retrofit.create(HttpService.class);
-//            Observable<HttpResult<Cert>> observable = apiService.downloadCert();
-//            observable.subscribeOn(Schedulers.io()).unsubscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
-//                    .subscribe(
-//                            new Subscriber<HttpResult<Cert>>() {
-//
-//                                @Override
-//                                public void onNext(HttpResult<Cert> certHttpResult) {
-//                                    if (certHttpResult != null)
-//                                        LogUtil.d("onNext = data= " + certHttpResult.getData().getData());
-//                                }
-//
-//                                @Override
-//                                public void onCompleted() {
-//                                    LogUtil.d("onCompleted ... ");
-//                                }
-//
-//                                @Override
-//                                public void onError(Throwable t) {
-//                                    LogUtil.e("onError = " + t.getMessage());
-//                                }
-//
-//                            }
-//                    );
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    LogUtil.d("success onResponse = \n" + response.body().string());
+                }
+            });
+
         } catch (Exception e) {
             e.printStackTrace();
         }
